2adbfce2ce9c2efac423c2369e5d6dd036f7b42d
itzAamir <amirpc190320@gmail.com>
2024-04-17 16:20:57 +0530

    initialized landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,27 @@ import React from "react";
 
 const Page = () => {
   return (
-    <main className="flex h-screen flex-col items-center justify-center bg-white p-2 text-black">
-      <Image src={IMAGES.logo} alt="..." height={400} width={400} />
-
-      <p>
-        Edit <code>src/app/page.tsx</code> and save to reload.
+    <main className="flex h-screen flex-col items-center justify-center bg-gray-900 p-2 text-gray-100">
+      <h1 className="text-center text-8xl font-bold text-blue-500">Wit-Pro</h1>
+      <p className="mt-4 text-center text-lg">
+        A drag and drop programming kit for Arduino that <br /> generates code
+        for you using a simple block-based interface.
       </p>
+
+      <div className="mt-8 flex items-center space-x-4">
+        <a
+          href=""
+          className="rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600"
+        >
+          Download for Windows
+        </a>
+        <a
+          href=""
+          className="rounded-md bg-gray-500 px-4 py-2 text-white hover:bg-gray-600"
+        >
+          Download for Mac
+        </a>
+      </div>
     </main>
   );
 };
